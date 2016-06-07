739530d02ac5da44e77ef136324568ae73315e2a
Fabien AUDO <fabien.audo@niji.fr>
2016-06-07 17:48:29 +0200

    Add plugin entry point

---
+++ rootpress-cli.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Rootpress CLI
+ * Plugin URI: http://www.niji.fr
+ * Description: A set of WP-CLI commands to support the rootpress MVC plugin integration
+ * Version: 0.1.0
+ * Author: Fabien AUDO, Maxime TUAL
+ * Text Domain: rootpress-cli
+ * Domain path: /languages/
+ */
